03c21d72e75fc9aaff2e3ed8f6768559959ab152
xy <exy2000a@163.com>
2020-03-28 15:53:11 +0800

    注册验证码

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,10 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'captcha' => ['required', 'captcha'],
+        ], [
+            'captcha.required' => '请输入验证码',
+            'captcha.captcha' => '验证码错误',
         ]);
     }
 
