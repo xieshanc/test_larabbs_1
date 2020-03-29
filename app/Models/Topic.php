d3c3e1c20ec4da65f5c4f2c255834a746333ce2a
xy <exy2000a@163.com>
2020-03-29 16:13:51 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'excerpt'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,50 @@
+@if (count($topics))
+  <ul class="list-unstyled">
+    @foreach ($topics as $topic)
+      <li class="media">
+
+        <div class="media-left">
+          <a href="{{ route('users.show', $topic->user_id) }}">
+            <img src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}" alt="" class="media-object img-thumbnail mr-3" style="width: 52px; height: 52px;">
+          </a>
+        </div>
+
+        <div class="media-body">
+
+          <div class="media-heading mt-0 mb-1">
+            <a href="{{ route('topics.show', $topic->id) }}" title="{{ $topic->title }}">
+              {{ $topic->title }}
+            </a>
+            <a href="{{ route('topics.show', $topic->id) }}" class="float-right">
+              <span class="badge badge-secondary badge-pill"> {{ $topic->reply_count }} </span>
+            </a>
+          </div>
+
+          <small class="media-body meta text-secondary">
+            <a href="#" class="text-secondary" title="{{ $topic->category->name }}">
+              <i class="far fa-folder"></i>
+              {{ $topic->category->name }}
+            </a>
+            <span> • </span>
+            <a href="{{ route('users.show', $topic->user_id) }}" title="{{ $topic->user->name }}" class="text-secondary">
+              <i class="far fa-user"></i>
+              {{ $topic->user->name }}
+            </a>
+            <span> • </span>
+            <i class="far fa-clock"></i>
+            <span class="timeago" title="最后活跃于：{{ $topic->updated_at }}">{{ $topic->updated_at }}</span>
+          </small>
+
+        </div>
+
+      </li>
+
+      @if (!$loop->last)
+        <hr>
+      @endif
+
+    @endforeach
+  </ul>
+@else
+  <div class="empty-block">你看你马呢</div>
+@endif
