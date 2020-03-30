9fd3e5f50c5a1f0255aa1e4fd84751a519c57092
xy <exy2000a@163.com>
2020-03-31 00:09:23 +0800

    用户最后活跃的时间

---
+++ app/Models/Traits/LastActivedAtHelper.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models\Traits;
+
+use Redis;
+use Carbon\Carbon;
+
+trait LastActivedAtHelper
+{
+    protected $hash_prefix = 'larabbs_last_actived_at_';
+    protected $field_prefix = 'user_';
+
+    // 存个人的数据
+    public function recordLastActivedAt()
+    {
+        $hash = $this->getHashFromDateString(Carbon::now()->toDateString());
+        $field = $this->getHashField();
+        $now = Carbon::now()->toDateTimeString();
+        Redis::hSet($hash, $field, $now);
+    }
+
+    // 计划任务，把数据保存到数据库
+    public function syncUserActivedAt()
+    {
+        $hash = $this->getHashFromDateString(Carbon::yesterday()->toDateString());
+        $dates = Redis::hGetAll($hash);
+        foreach ($dates as $user_id => $actived_at) {
+            $user_id = str_replace($this->field_prefix, '', $user_id);
+            if ($user = $this->find($user_id)) {
+                $user->last_actived_at = $actived_at;
+                $user->save();
+            }
+        }
+        Redis::del($hash);
+    }
+
+    // 取数据
+    public function getLastActivedAtAttribute($value)
+    {
+        $hash = $this->getHashFromDateString(Carbon::now()->toDateString());
+
+        $field = $this->getHashField();
+
+        $datetime = Redis::hGet($hash, $field) ?: $value;
+
+        if ($datetime) {
+            return new Carbon($datetime);
+        } else {
+            return $this->created_at;
+        }
+    }
+
+    public function getHashFromDateString($date)
+    {
+        return $this->hash_prefix . $date;
+    }
+
+    public function getHashField()
+    {
+        return $this->field_prefix . $this->id;
+    }
+}
