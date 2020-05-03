d5c9dfb8aa65e492ff60c0d6ef02bf651bcd8636
xy <exy2000a@163.com>
2020-05-03 18:00:04 +0800

    m

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,10 +18,11 @@ class UsersTableSeeder extends Seeder
 
         User::insert($user_array);
 
+        $faker = app(Faker\Generator::class);
         $user1 = User::find(1);
-        $user1->name = 'sunge';
+        $user1->name = $faker->name;
         $user1->email = 'exy2000a@163.com';
-        $user1->avatar = 'http://larabbs1.local/uploads/images/avatars/202003/1_1585467489_oDDaVkRClX.jpg';
+        $user1->avatar = config('app.url') . '/images/black.jpg';
         $user1->save();
 
         $user1->assignRole('Founder');
